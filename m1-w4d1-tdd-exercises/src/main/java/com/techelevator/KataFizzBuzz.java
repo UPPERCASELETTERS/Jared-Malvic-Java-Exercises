2dccba4b067f42cbc9cccdeac1a640d689b0d225
Jared Malvic <jarediscool92@gmail.com>
2017-09-18 16:11:35 -0400

    Finished FizzBuzz

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,28 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
-
+	
+	public String fizzBuzz(int n) {
+	String converted = String.valueOf(n);
+		
+		if (n <= 0) {
+		return "";
+		}
+	
+		if (n>100) {
+		return "";
+		}
+	
+		if (converted.contains("3") || (n % 3 == 0)) {
+		converted = "Fizz";
+		}
+	
+		if (converted.contains("5") || n % 5 == 0) {
+		converted = "Buzz";
+		}
+	
+		if (n % 15 == 0) {
+		converted = "FizzBuzz";
+		} return converted;	
+	}
 }
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,66 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	KataFizzBuzz sut = new KataFizzBuzz();
+	
+	@Test
+	public void testPrint1() {		
+		assertEquals("1", sut.fizzBuzz(1) );
+	}
+	
+	@Test
+	public void testPrint3() {		
+		assertEquals("Fizz", sut.fizzBuzz(3));
+	}
+	
+	@Test
+	public void testPrint5() {
+		assertEquals("Buzz", sut.fizzBuzz(5));
+	}
+	
+	@Test
+	public void testMulitpleOf3() {
+		assertEquals("Fizz", sut.fizzBuzz(33));
+	}
 
+	@Test
+	public void testMulitpleOf5() {
+		assertEquals("Buzz", sut.fizzBuzz(55));
+	}
+	
+	@Test
+	public void testMultipleOfBoth() {
+		assertEquals("FizzBuzz",sut.fizzBuzz(45));
+	}
+	
+	@Test 
+	public void testHas3NotDivisbleBy3() {
+		assertEquals("Fizz", sut.fizzBuzz(13));
+	}
+	
+	@Test 
+	public void testHas5NotDivisbleBy5() {
+		assertEquals("Fizz", sut.fizzBuzz(51));
+	}
+	
+	@Test
+	public void testZero() {
+		assertEquals("", sut.fizzBuzz(0));
+	}
+	
+	@Test
+	public void testLessThanZero() {
+		assertEquals("", sut.fizzBuzz(-1));
+	}
+	
+	@Test
+	public void test100() {
+		assertEquals("Buzz", sut.fizzBuzz(100));
+	}
+	
+	@Test
+	public void testGreaterThan100() {
+		assertEquals("", sut.fizzBuzz(101));
+	}
 }
