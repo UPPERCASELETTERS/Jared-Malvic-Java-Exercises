8a8f3e9b1061a155ce232b8badaa06d933b5a09e
Jared Malvic <jarediscool92@gmail.com>
2017-09-29 11:03:33 -0400

    Progress on w5d4

--- m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises.sql
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises.sql
@@ -2,19 +2,37 @@
 -- The following changes are applied to the "pagila" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
-
+INSERT INTO actor (first_name, last_name) VALUES ('Hampton', 'Avenue');
+INSERT INTO actor (first_name, last_name) VALUES ('Lisa', 'Byway');
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in 
 -- ancient Greece", to the film table. The movie was released in 2008 in English. 
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special 
 -- features, the film speaks for itself, and doesn't need any gimmicks.	
-
+INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, replacement_cost)
+VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in 
+-- ancient Greece', 2008, 1, 7, 1.99, 14.99);
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly 
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
-
+SELECT title, film_id FROM film
+WHERE title = 'Euclidean PI' ;
+-- (1001)
+SELECT first_name, last_name, actor_id FROM actor
+WHERE first_name = 'Lisa';
+-- (202)
+SELECT first_name, last_name, actor_id FROM actor
+WHERE first_name = 'Hampton';
+-- (201)
+INSERT INTO film_actor (actor_id, film_id) VALUES (202, 1001);
+INSERT INTO film_actor (actor_id, film_id) VALUES (201, 1001);
 -- 4. Add Mathmagical to the category table.
-
+INSERT INTO category (name) VALUES ('Mathmagical');
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI", 
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+BEGIN TRANSACTION;
+
+SELECT title, film_id FROM film
+WHERE title = 'Euclidean PI' ;
+
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films 
 -- accordingly.
