1759c20b5e173d6d4c55616629275faf2bfc105c
Jared Malvic <jarediscool92@gmail.com>
2017-09-29 07:52:19 -0400

    w5d3

--- m2-w5d3-joins-exercises/joins-exercises.sql
+++ m2-w5d3-joins-exercises/joins-exercises.sql
@@ -2,60 +2,152 @@
 
 -- 1. All of the films that Nick Stallone has appeared in
 -- (30 rows)
-
+SELECT f.title FROM film f
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.last_name = 'STALLONE';
 -- 2. All of the films that Rita Reynolds has appeared in
 -- (20 rows)
-
+SELECT f.title FROM film f
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.last_name = 'REYNOLDS';
 -- 3. All of the films that Judy Dean or River Dean have appeared in
 -- (46 rows)
-
+SELECT f.title FROM film f
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.last_name = 'DEAN';
 -- 4. All of the the ‘Documentary’ films
 -- (68 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Documentary';
 -- 5. All of the ‘Comedy’ films
 -- (58 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Comedy';
 -- 6. All of the ‘Children’ films that are rated ‘G’
 -- (10 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Children' AND rating = 'G';
 -- 7. All of the ‘Family’ films that are rated ‘G’ and are less than 2 hours in length
 -- (3 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Family' AND rating = 'G' AND length < 120;
 -- 8. All of the films featuring actor Matthew Leigh that are rated ‘G’
 -- (9 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.last_name = 'LEIGH' AND rating = 'G' AND a.first_name = 'MATTHEW'
 -- 9. All of the ‘Sci-Fi’ films released in 2006
 -- (61 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE release_year = 2006 AND c.name = 'Sci-Fi';
 -- 10. All of the ‘Action’ films starring Nick Stallone
 -- (2 rows)
-
+SELECT f.title FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.last_name = 'STALLONE' AND c.name = 'Action' AND a.first_name = 'NICK';
 -- 11. The address of all stores, including street address, city, district, and country
 -- (2 rows)
-
+SELECT address, city, district, country  FROM address a
+JOIN store s ON a.address_id = s.address_id
+JOIN city c ON a.city_id = c.city_id
+JOIN country co ON c.country_id = co.country_id;
 -- 12. A list of all stores by ID, the store’s street address, and the name of the store’s manager
 -- (2 rows)
-
+SELECT store_id, address, city, district, country, manager_staff_id  FROM address a
+JOIN store s ON a.address_id = s.address_id
+JOIN city c ON a.city_id = c.city_id
+JOIN country co ON c.country_id = co.country_id;
 -- 13. The first and last name of the top ten customers ranked by number of rentals 
 -- (#1 should be “ELEANOR HUNT” with 46 rentals, #10 should have 39 rentals)
-
+SELECT first_name, last_name, COUNT(rental_id) AS rental_count FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY c.first_name, c.Last_name
+ORDER BY rental_count DESC
+LIMIT 10;
 -- 14. The first and last name of the top ten customers ranked by dollars spent 
 -- (#1 should be “KARL SEAL” with 221.55 spent, #10 should be “ANA BRADLEY” with 174.66 spent)
-
+SELECT first_name, last_name, sum(p.amount) FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY p.customer_id, last_name, first_name
+ORDER BY sum(p.amount) DESC
+LIMIT 10;
 -- 15. The store ID, street address, total number of rentals, total amount of sales (i.e. payments), and average sale of each store 
--- (Store 1 has 7928 total rentals and Store 2 has 8121 total rentals)
-
+-- (Store 1 has 7928 total rentals and Store 2 has 8121 total rentals) OR 15866      ** BROKEN **
+SELECT str.store_id, address, p.rental_id, sum(p.amount) FROM store str
+JOIN staff sff ON str.store_id = sff.store_id
+JOIN rental r ON sff.staff_id = r.staff_id
+JOIN payment p ON r.staff_id = p.staff_id
+JOIN address a ON sff.address_id = a.address_id
+GROUP BY str.store_id, a.address, p.rental_id
 -- 16. The top ten film titles by number of rentals
 -- (#1 should be “BUCKET BROTHERHOOD” with 34 rentals and #10 should have 31 rentals)
-
+SELECT title, count (title) FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY title
+ORDER BY count(title) DESC
+LIMIT 10;
 -- 17. The top five film categories by number of rentals 
 -- (#1 should be “Sports” with 1179 rentals and #5 should be “Family” with 1096 rentals)
-
+SELECT c.name, count (c.category_id) FROM category c 
+JOIN film_category fc ON c.category_id = fc.category_id
+JOIN inventory i ON fc.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY c.category_id
+ORDER BY count(c.category_id) DESC
+LIMIT 5;
 -- 18. The top five Action film titles by number of rentals 
 -- (#1 should have 30 rentals and #5 should have 28 rentals)
-
+SELECT title, count (title) FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON c.category_id = fc.category_id
+JOIN inventory i ON fc.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE c.name = 'Action'
+GROUP BY f.title
+ORDER BY count(title) DESC
+LIMIT 5;
 -- 19. The top 10 actors ranked by number of rentals of films starring that actor 
 -- (#1 should be “GINA DEGENERES” with 753 rentals and #10 should be “SEAN GUINESS” with 599 rentals)
+SELECT a.first_name, a.last_name, count (rental_id) FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY count(rental_id) DESC
+LIMIT 5;
 
 -- 20. The top 5 “Comedy” actors ranked by number of rentals of films in the “Comedy” category starring that actor 
 -- (#1 should have 87 rentals and #5 should have 72 rentals)
+SELECT a.first_name, a.last_name, count (rental_id) FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON c.category_id = fc.category_id
+WHERE c.name = 'Comedy'
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY count(rental_id) DESC
+LIMIT 5;
